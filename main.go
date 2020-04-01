6271254e815b5c6902fc076989e396db2dadc931
johnbuonassisi <johnbuonassisi@gmail.com>
2020-04-01 04:31:13 -0700

    Update to root server

--- main.go
+++ main.go
@@ -3,12 +3,36 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
+
+// FileSystem custom file system handler
+type FileSystem struct {
+	fs http.FileSystem
+}
+
+// Open opens file
+func (fs FileSystem) Open(path string) (http.File, error) {
+	f, err := fs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := strings.TrimSuffix(path, "/") + "/index.html"
+		if _, err := fs.fs.Open(index); err != nil {
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
 
 func main() {
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(FileSystem{http.Dir("./static")})
+	http.Handle("/", fs)
 
 	log.Println("Listening on :80...")
 	err := http.ListenAndServe(":80", nil)
